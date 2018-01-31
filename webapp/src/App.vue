5b307c6048f732e909f04ca397651e450543a1fc
Daniel Machado <daniel.machado@domrock.com.br>
2018-01-31 14:24:25 -0200

    Apresentacao video login

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -7,23 +7,37 @@
     <dr-toolbar-sub v-if="$store.state.isUserLoggedIn">
     </dr-toolbar-sub>
     <v-content>
-      <div v-if="!$store.state.isUserLoggedIn" class="bg">
-        <v-fade-transition mode="out-in">
-          <router-view></router-view>
-        </v-fade-transition>
+
+      <div v-if="!$store.state.isUserLoggedIn" id="fullScreenDiv">
+          <img src="/static/bg_prin.png" id="videosubstitute" alt="Full screen background video">
+          <div id="videoDiv">           
+              <video preload="preload" id="video" autoplay="autoplay" loop="loop">
+              <source src="/static/636796213.mp4" type="video/mp4"></source>
+              </video> 
+          </div>
+          <div id="filterBox"> 
+          </div> 
+          <div id="messageBox"> 
+              <div>
+          <v-fade-transition mode="out-in">
+            <router-view></router-view>
+          </v-fade-transition>
+              </div>
+          </div>   
       </div>
+      
       <v-container v-if="$store.state.isUserLoggedIn" fluid >
           <!-- <center>
             <v-btn icon @click.stop="drawer = !drawer">
               <v-icon style="font-size:22px;color:#8f8f9b">sort</v-icon>
             </v-btn>
           </center> -->
-
         <v-fade-transition mode="out-in">
           <router-view></router-view>
         </v-fade-transition>
       </v-container>
       <dr-footer :loggedIn="!$store.state.isUserLoggedIn"></dr-footer>
+
     </v-content>
     <dr-navigation-overlaping v-if="toolbarItens.removeNavOverlaping && $store.state.isUserLoggedIn" :toolbarItens="toolbarItens">
     </dr-navigation-overlaping>
